44a5ab556593d46096a98a4ed9a2b2db49f62433
phartenfeller <pharti@hotmail.de>
2022-11-20 11:15:15 +0100

    run benchmark from da preps

--- src/index.js
+++ src/index.js
@@ -1,71 +1,60 @@
 import Worker from "worker-loader!./opfs-worker.js";
 
-const testRows = document.querySelectorAll("tbody tr");
-const button = /** @type {HTMLButtonElement} */ (
-  document.getElementById("start")
-);
-const preamble = /** @type {HTMLButtonElement} */ (
-  document.getElementById("preamble")
-);
-const error = document.getElementById("error");
+const preamble = `-- Pre-run setup
+PRAGMA journal_mode=delete;`;
 
 const worker = new Worker();
 worker.addEventListener(
-  "message",
-  function () {
-    button.disabled = false;
-  },
-  { once: true }
+	"message",
+	function () {
+		console.log("message received");
+	},
+	{ once: true },
 );
 
-button.addEventListener("click", async function () {
-  button.disabled = true;
-  preamble.disabled = true;
-  error.textContent = "";
-
-  // Clear previous results.
-  for (const row of testRows) {
-    while (row.childElementCount > 1) {
-      row.removeChild(row.lastChild);
-    }
-  }
-
-  try {
-    const rows = Array.from(testRows);
-    for await (const result of benchmark()) {
-      const td = document.createElement("td");
-      td.textContent = `${result / 1000} s`;
-      rows.shift().append(td);
-    }
-  } finally {
-    button.disabled = false;
-    preamble.disabled = false;
-  }
-});
+async function runBench() {
+	for await (const result of benchmark()) {
+		const res = `${result / 1000} s`;
+		console.log("res", res);
+	}
+}
 
 async function* benchmark() {
-  await request({
-    f: "initialize",
-    preamble: preamble.value,
-  });
+	await request({
+		f: "initialize",
+		preamble: preamble,
+	});
 
-  for (let i = 0; i < testRows.length; ++i) {
-    const result = await request({ f: "test", i });
-    yield result;
-  }
+	for (let i = 0; i < 16; ++i) {
+		const result = await request({ f: "test", i });
+		yield result;
+	}
 
-  await request({ f: "finalize" });
+	await request({ f: "finalize" });
 }
 
 function request(message) {
-  worker.postMessage(message);
-  return new Promise(function (resolve) {
-    worker.addEventListener(
-      "message",
-      function ({ data }) {
-        resolve(data);
-      },
-      { once: true }
-    );
-  });
+	worker.postMessage(message);
+	return new Promise(function (resolve) {
+		worker.addEventListener(
+			"message",
+			function ({ data }) {
+				resolve(data);
+			},
+			{ once: true },
+		);
+	});
+}
+
+if (!window.hartenfeller_dev) {
+	window.hartenfeller_dev = {};
+}
+if (!window.hartenfeller_dev.plugins) {
+	window.hartenfeller_dev.plugins = {};
+}
+if (!window.hartenfeller_dev.plugins.sync_offline_data) {
+	window.hartenfeller_dev.plugins.sync_offline_data = {};
+}
+if (!window.hartenfeller_dev.plugins.sync_offline_data.sync) {
+	window.hartenfeller_dev.plugins.sync_offline_data.sync = runBench;
 }
